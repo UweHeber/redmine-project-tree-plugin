dffe719599374e74983a458e4360a84a3ff2a5d8
Uwe Heber <uwe@heber.it>
2019-01-08 18:21:41 +0100

    solved display issue for archived parent projects in Tree-Overview, replaced tabs with spaces

--- lib/project_tree/patches/projects_controller_patch.rb
+++ lib/project_tree/patches/projects_controller_patch.rb
@@ -5,11 +5,11 @@ module ProjectTree
         module ProjectsControllerPatch
         
             def self.included(base) # :nodoc:
-                base.send(:include, InstanceMethods)                
+                base.send(:include, InstanceMethods)
                 base.class_eval do
                     unloadable
                     #the before_filter is who gives the authorization to execute the function
-					before_filter :authorize, :except => [ :index, :children, :autocomplete, :list, :new, :create, :copy ]
+                    before_filter :authorize, :except => [ :index, :children, :autocomplete, :list, :new, :create, :copy, :archive, :unarchive, :destroy ]
                 end
             end
             
@@ -24,13 +24,16 @@ module ProjectTree
                     # encapsulates all children of current project
                     children = []
                     
-                    project.children.each do |child|                        
-                        entry = {}
-                        entry[:id] = child.id
-                        entry[:name] = child.name
-                        entry[:identifier] = child.identifier
-                        entry[:has_children] = (child.children.length > 0) ? true : false
-                        children << entry
+                    project.children.each do |child|
+                        # return only active projects to the result
+                        if child.status == 1
+                            entry = {}
+                            entry[:id] = child.id
+                            entry[:name] = child.name
+                            entry[:identifier] = child.identifier
+                            entry[:has_children] = (child.children.length > 0) ? true : false
+                            children << entry
+                        end
                     end
                     # add children to the result
                     result[:children] = children
