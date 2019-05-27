98d60d73c5529a7e88ae48f04d7be8686db50f52
Uwe Heber <36344136+UweHeber@users.noreply.github.com>
2019-05-27 15:59:40 +0200

    Update init.rb
    Increase version after change the ordering of top level projects

--- init.rb
+++ init.rb
@@ -8,7 +8,7 @@ Redmine::Plugin.register :project_tree do
 	name 'Project Tree plugin'
 	author 'Uwe Heber, Matthias Petermann'
 	description 'This is a Redmine plugin which will turn the projects page into a tree view'
-	version '1.0.2'
+	version '1.0.3'
 	url 'https://github.com/UweHeber/redmine-project-tree-plugin'
 	author_url 'http://heber.it/about, http://petermann-it.de'
 end
